85d5b22e9333c9bd5983d66267b59f12905fb9a0
IanKaremi <71334186+IanKaremi@users.noreply.github.com>
2022-02-24 04:28:57 +0300

    registration

---
+++ validation.js
@@ -0,0 +1,14 @@
+const nameInput = document.querySelector('input');
+
+nameInput.addEventListener('input', () => {
+  nameInput.setCustomValidity('');
+  nameInput.checkValidity();
+});
+
+nameInput.addEventListener('invalid', () => {
+  if(nameInput.value === '') {
+    nameInput.setCustomValidity('Enter your username!');
+  } else {
+    nameInput.setCustomValidity('Usernames can only contain upper and lowercase letters. Try again!');
+  }
+});
