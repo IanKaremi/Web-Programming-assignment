1a4e4f04a6ec3c404a125ed651b9464aa460f88b
IanKaremi <71334186+IanKaremi@users.noreply.github.com>
2022-04-11 14:37:11 +0300

    php functionality

--- records.php
+++ records.php
@@ -7,12 +7,20 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Afya Hospital - Home</title>
+        <title>Afya Hospital - Records</title>
         <meta name="description" content="">
         <meta name="theme-color" content="cyan">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="web_assign.css">
         <link rel="stylesheet" href="records.css">
+        <style type="text/css">
+            table{ border-collapse: collapse; width:100%; border: solid 2px;}
+            th {background-color:white; color:black;}
+            td {padding:10px; border: solid 2px;}
+            tr:nth-of-type(even){background-color:cyan;}
+            tr:nth-of-type(odd){background-color:white;}
+
+        </style>
     </head>
     <body>
         <!--[if lt IE 7]>
@@ -23,7 +31,7 @@
         <header>
             <div><a href="index.html">Home</a></div>
             <div><a href="Registration.html">Registration</a></div>
-            <div><a href="records.html">Records</a></div>
+            <div><a href="/Afya/records.php">Records</a></div>
             <div><a href="about_us.html">About Us</a></div>
             <div><a href="contacts.html">Contacts</a></div>
         </header>
@@ -47,7 +55,65 @@
 
         <div class="content">
             <h1>Patient Records</h1>
-            <table class="table">
+            <?php
+              echo "<link rel='stylesheet' type='text/css' href='Afya/records.css' />";
+            $con= mysqli_connect("localhost","root","","afya_hosp") or die("Failed to connect.");
+            
+            $read="SELECT * FROM patients";
+
+            $id = $_REQUEST['id'];
+            $fname = $_REQUEST['fname'];
+            $mname = $_REQUEST['mname'];
+            $sname = $_REQUEST['sname'];
+            $dob = $_REQUEST['dob'];
+            $gender = $_REQUEST['gender'];
+            $county = $_REQUEST['county'];
+            $kin = $_REQUEST['kin'];
+          
+            $write = "INSERT INTO patients  VALUES ('$id', '$fname', '$mname','$sname', '$dob','$gender' ,'$county', '$kin')";
+
+            if(mysqli_query($con, $write)){
+                echo "<h3>Data stored in database successfully!<br>" 
+                    . " Please check the table" 
+                    . " to view the updated data.</h3>"; 
+    
+            }/*else{
+                echo("Warning!".mysqli_error($con). "<br>");
+            }*/
+            
+            echo "Displaying data: <br><br>";
+            $result = mysqli_query($con, $read) ;
+
+            if(mysqli_num_rows($result) > 0)
+            {
+                echo"<table><tr><th>Patient ID</th><th>First Name</th><th>Middle Name</th><th>Surname</th><th>DOB</th><th>Gender</th><th>County</th><th>Kin</th></tr>";
+                while($row = $result->fetch_assoc()) {
+                    echo "<tr><td>"
+                    .$row['Patient_ID']."   "
+                    ."</td><td>"
+                    .$row['First_Name']."    "
+                    ."</td><td>"
+                    .$row['Middle_Name']."    "
+                    ."</td><td>"
+                    .$row['Surname']."    "
+                    ."</td><td>"
+                    .$row['Date_of_Birth']."    "
+                    ."</td><td>"
+                    .$row['Gender']."    "
+                    ."</td><td>"
+                    .$row['County']."    "
+                    ."</td><td>"
+                    .$row['Next_of_Kin']
+                    ."</td></tr>";
+                }
+                echo"</table><br> <br>";
+           }else{
+                echo"0 results";
+            };
+            mysqli_close($con); 
+
+            ?>
+           <!-- <table class="table">
                     <tr class="odd" id="head">
                     <td>Patient ID</td>
                     <td>First Name</td>
@@ -95,7 +161,7 @@
                     <td>Cell</td>
                     <td>Cell</td>
                     <td>Cell</td>
-                    </tr>
+                    </tr> -->
                 
                 </table>
     
